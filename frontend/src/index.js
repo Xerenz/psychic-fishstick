d6408d3c7e9fa0b849e46d8672784f70b35a317e
ern75858 <ern75858@adobe.com>
2023-03-12 21:05:18 +0530

    Initial frontend layouts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,31 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Landing from './views/Landing';
+import Login from './views/Login';
+import Register from './views/Register';
 import './index.css';
-import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
