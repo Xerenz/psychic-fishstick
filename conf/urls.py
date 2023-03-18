684c06b6794f41842fab41066c621177343d9d4c
ern75858 <ern75858@adobe.com>
2023-03-18 17:49:39 +0530

    Modified conf urls to add users urls

--- conf/urls.py
+++ conf/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('social_django.urls', namespace='social')),
+    # path('', include('social_django.urls', namespace='social')),
+    path('api/users/', include('apps.users.urls')),
     path('admin/', admin.site.urls),
 ]
