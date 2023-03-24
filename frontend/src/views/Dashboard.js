5e7a35da4c2d77e8cb5ee99210e27ab7e8af1497
ern75858 <ern75858@adobe.com>
2023-03-24 14:10:36 +0530

    Invite and Schedule frames

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Container } from '@mui/system';
 import Authenticate from '../components/Authenticate'
 import { Navbar } from '../components/Navbar';
 import Hobbies from '../components/Hobbies';
 import Create from '../components/Create';
+import Schedule from '../components/Schedule';
+import Invite from '../components/Invite';
 
 export default function Dashboard() {
   return (
@@ -16,6 +18,10 @@ export default function Dashboard() {
                 element={<Hobbies />} />
                 <Route path='create-event'
                 element={<Create />} />
+                <Route path='schedule/:id'
+                element={<Schedule />} />
+                <Route path='invite/:id'
+                element={<Invite />} />
             </Routes>
         </Container>
     </Authenticate>
